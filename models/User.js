e055063fe3a96d7be62e35244ca8bdb573271fdf
Juan Reyes <camiloreyes1125@gmail.com>
2024-02-05 23:03:29 -0500

    user schema commit

---
+++ models/User.js
@@ -0,0 +1,23 @@
+const { Schema, model, Types } = require("mongoose")
+
+const userSchema = new Schema(
+    {
+        fullName: { type: String,
+                   required: true },
+            
+        email: { type: String,
+                 required: true,
+                 unique: true},
+        
+        password: { type: String,
+                    required: true}
+                
+    },
+
+    {
+        timeseries: true
+    }
+
+)
+
+module.exports = model("User", userSchema);
