6e3a8feaab88d9f20aee35ff034d69c715530c55
Juan Reyes <camiloreyes1125@gmail.com>
2024-02-15 20:40:06 -0500

    user detail route

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+const User = require('../models/User');
+
+router.get('/user-detail/:userId', (req,res,next => {
+
+  const { userId } = req.params;
+
+  User.findById(userId)
+    .then((foundUser) => {
+      res.json(foundUser)
+    })
+    .catch((err) => {
+      console.log(err)
+      next(err)
+    })
+}))
+
+
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
